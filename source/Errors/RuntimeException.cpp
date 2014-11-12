52e5374443218f6e04980f27eb89e5f5e6d56365
raizenpk <glaedrthewise@gmail.com>
2014-11-12 12:25:04 +0200

    Added RunException

---
+++ source/Errors/RuntimeException.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <exception>
+using namespace std;
+
+namespace Pixeltwo {
+  class ERuntimeException: public exception {
+
+    private:
+      const char* ErrorMessage;
+
+    public:
+
+      ERuntimeException(const char* anErrorMessage) throw() {
+        this->ErrorMessage = anErrorMessage;
+      }
+
+      ~ERuntimeException() throw() {
+        delete this->ErrorMessage;
+      };
+
+      virtual const char* what() const throw() {
+        char* error = new char[100];
+        sprintf(error, "Runtime Exception: %s\n", this->ErrorMessage);
+        return error;
+      }
+  };
+}
+
