6f372a72493aed5c8f154b09c9d8b53c89ce1538
Essar <glaedrthewise@gmail.com>
2017-02-17 17:27:38 +0200

    Anisotropic texture filtering.

--- source/Ess2D/IOManager.cpp
+++ source/Ess2D/IOManager.cpp
@@ -30,9 +30,9 @@ namespace Ess2D {
     glBindTexture(GL_TEXTURE_2D, texture._id);
     glTexImage2D(GL_TEXTURE_2D, 0, texture._mode, texture._width, texture._height, 0, texture._mode, GL_UNSIGNED_BYTE, surface->pixels);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     glGenerateMipmap(GL_TEXTURE_2D);
--- source/Skyline/FBORenderer.h
+++ source/Skyline/FBORenderer.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <Renderer.h>
 #include <Shader.h>
 
 class FBORenderer {
